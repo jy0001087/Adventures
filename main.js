16069342e268ed273d0e7d81dffd30589be50ba3
jy0001087@gmail.com <jy0001087@gmail.com>
2023-11-21 17:18:49 +0800

    可以打开窗口 todo: 无法引入showdown

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const { app,BrowserWindow } = require('electron')
+const path = require('node:path')
 
 const creatWindow = () => {
     const win = new BrowserWindow({
         width:800,
-        heght:600
+        heght:600,
+        webPreferences: {
+            preload: path.join(__dirname, 'showdown.min.js')
+          }
     })
 
-    win.loadURL('https://www.bing.com')
+    //win.loadURL('https://www.bing.com')
+    win.loadFile('UI/editor.html')
+    win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
     creatWindow()
 })
+
+//关闭窗口时，退出应用
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
