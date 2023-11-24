48cf04450829ab8b0500df108872b85c2b8f11ec
jy0001087@gmail.com <jy0001087@gmail.com>
2023-11-24 18:31:16 +0800

    增加新增待办事项 todo： 1.可以修改新待办名称 2.可以创建新待办名称的文件夹 3.待办文件夹内存储markdowm文档 4.待办文档内存储相关文件

--- UI/editor.js
+++ UI/editor.js
@@ -4,3 +4,14 @@ function compile(){
     var html = converter.makeHtml(text);
     document.getElementById("result").innerHTML = html;
 }
+
+
+window.onload=function(){
+    var newTodo = document.getElementById("newTodoList");
+    newTodo.onclick = function(){
+        var newTodoItem = document.createElement("div");
+        newTodoItem.innerHTML="新待办";
+        var todolistContent = document.getElementById("todolist-content")
+        todolistContent.appendChild(newTodoItem);
+    }
+}
