54043ed078af63350416ac6a56b762381596b0f5
jy0001087@gmail.com <jy0001087@gmail.com>
2023-12-01 14:12:17 +0800

    解决冲突问题

--- script/event/custElements.js
+++ script/event/custElements.js
@@ -1,17 +1,3 @@
-function TodoMenuItemDivElement() {
-    // 创建一个 div 元素作为对象的表示
-    this.element = document.createElement('div');
-    // 初始化一些属性
-}
-
-// 重写todolist-content元素的appendChild方法
-TodoMenuItemDivElement.prototype.appendChild = function (newChild) {
-
-    console.log("Before appending child:", newChild);
-    newChild.setAttribute("class","TodoMenuItem");
-    // 调用原始的appendChild方法
-    Element.prototype.appendChild.call(this, newChild);
-};
 // 扩展 HTMLElement 原型
 HTMLElement.prototype.todolistContentAppendChild = function(newChild) {
     // 在这里添加你的自定义函数逻辑
